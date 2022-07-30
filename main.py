01cad6eecdf1d6ab14dce844f60e1d6ad5d6c07b
Lorenzo Carrano <lorenzoc7297@gmail.com>
2022-07-30 16:23:02 +0200

    calcuations implemented; need to resolve the problem of visualizing capacitor values that are in the order of nF, otherwise thwy will be displayed as 0

--- main.py
+++ main.py
@@ -10,10 +10,11 @@ def parametersExtraction(inputList, outputList):
     
     opFreq = inputList[0]
     chImped = inputList[1]
-    S11 = inputList[2]
-    S12 = inputList[3]
-    S21 = inputList[4]
-    S22 = inputList[5]
+    SRF = inputList[2]
+    S11 = inputList[3]
+    S12 = inputList[4]
+    S21 = inputList[5]
+    S22 = inputList[6]
     
     A = outputList[0]
     B = outputList[1]
@@ -77,8 +78,25 @@ def parametersExtraction(inputList, outputList):
     Y2.delete(0, END)
     Y2.insert(0, valueY2Rounded)
     Y2.configure(state="disabled")
-    valueC1 = valueY1.imag()/(2*pi*int(opFreq.get()))
-    valueC2 = valueY2.imag()/(2*pi*int(opFreq.get()))
+    commonDen = (2*pi*int(opFreq.get()))
+    valueC1 = valueY1.imag/commonDen
+    valueC1Rounded = complex(round(valueC1.real, round_digits), round(valueC1.imag, round_digits))
+    C1.configure(state="normal")
+    C1.delete(0, END)
+    C1.insert(0, valueC1Rounded)
+    C1.configure(state="normal")
+    valueC2 = valueY2.imag/commonDen
+    valueC2Rounded = complex(round(valueC2.real, round_digits), round(valueC2.imag, round_digits))
+    C2.configure(state="normal")
+    C2.delete(0, END)
+    C2.insert(0, valueC2Rounded)
+    C2.configure(state="normal")
+    valueC3 = 1/(Inductance*((2*pi)*int(SRF.get()))*((2*pi)*int(SRF.get())))
+    valueC3Rounded = complex(round(valueC3.real, round_digits), round(valueC3.imag, round_digits))
+    C3.configure(state="normal")
+    C3.delete(0, END)
+    C3.insert(0, valueC3Rounded)
+    C3.configure(state="normal")
     
     
 def reset(inputList, outputList):
@@ -101,17 +119,20 @@ def main():
     #input frame 
     InputFrame = LabelFrame(root, text="Input", font=("Helvetica", 10), background=frame_background_color)
     InputFrame.grid(row=0,column=0, sticky="n")
-    #operating frequency
     InputParameterFrame = LabelFrame(InputFrame, text="Electrical Parameters", font=("Helvetica", 10), background=options_background_color)
     InputParameterFrame.grid(row=0, column=0, sticky="w")
     opFreqLabel = Label(InputParameterFrame, text="Op Frequency", font=("Helvetica", 10), background=options_background_color, justify='left')
     opFreqLabel.grid(row=0, column=0, sticky='w')
     chImpedanceLabel = Label(InputParameterFrame, text="Ch. Impedance", font=("Helvetica", 10), background=options_background_color, justify='left')
     chImpedanceLabel.grid(row=1, column=0, sticky='w')
+    SRFLabel = Label(InputParameterFrame, text="SRF", font=("Helvetica", 10), background=options_background_color, justify='left')
+    SRFLabel.grid(row=2, column=0, sticky='w')
     opFreqEntr = Entry(InputParameterFrame, font=("Helvetica", 10), width=entry_width, justify="right")
     opFreqEntr.grid(row=0, column=1, sticky='w')
     chImpedanceEntr = Entry(InputParameterFrame, font=("Helvetica", 10), width=entry_width, justify="right")
     chImpedanceEntr.grid(row=1, column=1, sticky='w')
+    SRFEntr = Entry(InputParameterFrame, font=("Helvetica", 10), width=entry_width, justify="right")
+    SRFEntr.grid(row=2, column=1, sticky='w')
     #scattering matrix
     #labels
     scatteringMatrixFrame = LabelFrame(InputFrame, text="Scattering Matrix", font=("Helvetica", 10), background=options_background_color)
@@ -185,13 +206,14 @@ def main():
     #precharging default values in entries
     chImpedanceEntr.insert(0, "50")
     opFreqEntr.insert(0, "200000000")
+    SRFEntr.insert(0, "3350000000")
     scatteringMatrixS11Entry.insert(0, "(0.0246+0.1411j)")
     scatteringMatrixS12Entry.insert(0, "(0.9751-0.1644j)")
     scatteringMatrixS21Entry.insert(0, "(0.9751-0.1644j)")
     scatteringMatrixS22Entry.insert(0, "(0.0246+0.1411j)")
     
     #packaging of input and output parameters
-    inputParameters = [opFreqEntr, chImpedanceEntr, scatteringMatrixS11Entry, scatteringMatrixS12Entry, scatteringMatrixS21Entry, scatteringMatrixS22Entry]
+    inputParameters = [opFreqEntr, chImpedanceEntr, SRFEntr, scatteringMatrixS11Entry, scatteringMatrixS12Entry, scatteringMatrixS21Entry, scatteringMatrixS22Entry]
     outputParameters = [abcdMatrixAEntry, abcdMatrixBEntry, abcdMatrixCEntry, abcdMatrixDEntry, C1Entry, C2Entry, C3Entry, Y1Entry, Y2Entry]
     
     #calculation button
