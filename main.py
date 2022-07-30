6e616a2cee5ec3578d1a3e88590008761e27f230
Lorenzo Carrano <lorenzoc7297@gmail.com>
2022-07-30 13:52:41 +0200

    gui finished; calculations to be implemented

--- main.py
+++ main.py
@@ -2,26 +2,30 @@ from tkinter import *
 frame_background_color = "light gray"
 options_background_color = "light gray"
 root_background_color = "light gray"
+
+def parametersExtraction():
+    
+    return 0
 def main():
     #main window settings
     root = Tk()
     root.title("Parameters Extraction")
     root.configure(background=root_background_color)
-    root.geometry("500x700")
+    #root.geometry("500x700")
     
     #input frame 
     InputFrame = LabelFrame(root, text="Input", font=("Helvetica", 10), background=frame_background_color)
-    InputFrame.grid(row=0,column=0)
+    InputFrame.grid(row=0,column=0, sticky="n")
     #operating frequency
-    ParameterFrame = LabelFrame(InputFrame, text="Electrical Parameters", font=("Helvetica", 10), background=options_background_color)
-    ParameterFrame.grid(row=0, column=0)
-    opFreqLabel = Label(ParameterFrame, text="Op Frequency", font=("Helvetica", 10), background=options_background_color, justify='left')
+    InputParameterFrame = LabelFrame(InputFrame, text="Electrical Parameters", font=("Helvetica", 10), background=options_background_color)
+    InputParameterFrame.grid(row=0, column=0)
+    opFreqLabel = Label(InputParameterFrame, text="Op Frequency", font=("Helvetica", 10), background=options_background_color, justify='left')
     opFreqLabel.grid(row=0, column=0, sticky='w')
-    chImpedanceLabel = Label(ParameterFrame, text="Ch. Impedance", font=("Helvetica", 10), background=options_background_color, justify='left')
+    chImpedanceLabel = Label(InputParameterFrame, text="Ch. Impedance", font=("Helvetica", 10), background=options_background_color, justify='left')
     chImpedanceLabel.grid(row=1, column=0, sticky='w')
-    opFreqEntr = Entry(ParameterFrame, font=("Helvetica", 10), width=7, justify="right")
+    opFreqEntr = Entry(InputParameterFrame, font=("Helvetica", 10), width=7, justify="right")
     opFreqEntr.grid(row=0, column=1, sticky='w')
-    chImpedanceEntr = Entry(ParameterFrame, font=("Helvetica", 10), width=7, justify="right")
+    chImpedanceEntr = Entry(InputParameterFrame, font=("Helvetica", 10), width=7, justify="right")
     chImpedanceEntr.grid(row=1, column=1, sticky='w')
     #scattering matrix
     #labels
@@ -48,8 +52,31 @@ def main():
     #output frame
     OutputFrame = LabelFrame(root, text="Output", font=("Helvetica", 10), background=frame_background_color)
     OutputFrame.grid(row=0,column=1)
+    OutputParameterFrame = LabelFrame(OutputFrame, text="Components", font=("Helvetica", 10), background=options_background_color)
+    OutputParameterFrame.grid(row=0, column=0, sticky="w")
+    C1Label = Label(OutputParameterFrame, text="C1", font=("Helvetica", 10), background=options_background_color, justify='left')
+    C1Label.grid(row=0, column=0, sticky='w')
+    C2Label = Label(OutputParameterFrame, text="C2", font=("Helvetica", 10), background=options_background_color, justify='left')
+    C2Label.grid(row=1, column=0, sticky='w')
+    C3Label = Label(OutputParameterFrame, text="C3", font=("Helvetica", 10), background=options_background_color, justify='left')
+    C3Label.grid(row=2, column=0, sticky='w')
+    Y1Label = Label(OutputParameterFrame, text="Y1", font=("Helvetica", 10), background=options_background_color, justify='left')
+    Y1Label.grid(row=3, column=0, sticky='w')
+    Y2Label = Label(OutputParameterFrame, text="Y2", font=("Helvetica", 10), background=options_background_color, justify='left')
+    Y2Label.grid(row=4, column=0, sticky='w')
+    C1Entry = Entry(OutputParameterFrame, font=("Helvetica", 10), width=7, justify="right", state="disabled")
+    C1Entry.grid(row=0, column=1, sticky="w")
+    C2Entry = Entry(OutputParameterFrame, font=("Helvetica", 10), width=7, justify="right", state="disabled")
+    C2Entry.grid(row=1, column=1, sticky="w")
+    C3Entry = Entry(OutputParameterFrame, font=("Helvetica", 10), width=7, justify="right", state="disabled")
+    C3Entry.grid(row=2, column=1, sticky="w")
+    Y1Entry = Entry(OutputParameterFrame, font=("Helvetica", 10), width=7, justify="right", state="disabled")
+    Y1Entry.grid(row=3, column=1, sticky="w")
+    Y2Entry = Entry(OutputParameterFrame, font=("Helvetica", 10), width=7, justify="right", state="disabled")
+    Y2Entry.grid(row=4, column=1, sticky="w")
+    
     #ABCD matrix
-    #labels
+    #labels    
     abcdMatrixFrame = LabelFrame(OutputFrame, text="ABCD Matrix", font=("Helvetica", 10), background=options_background_color)
     abcdMatrixFrame.grid(row=1, column=0)
     abcdMatrixALabel = Label(abcdMatrixFrame, text="A", font=("Helvetica", 10), background=options_background_color, justify='left')
@@ -61,14 +88,17 @@ def main():
     abcdMatrixDLabel = Label(abcdMatrixFrame, text="D", font=("Helvetica", 10), background=options_background_color, justify='left')
     abcdMatrixDLabel.grid(row=1, column=2)
     #parameter entries
-    abcdMatrixAEntry = Entry(abcdMatrixFrame, font=("Helvetica", 10), width=7, justify="right")
+    abcdMatrixAEntry = Entry(abcdMatrixFrame, font=("Helvetica", 10), width=7, justify="right", state="disabled")
     abcdMatrixAEntry.grid(row=0, column=1, sticky="w")
-    abcdMatrixBEntry = Entry(abcdMatrixFrame, font=("Helvetica", 10), width=7, justify="right")
+    abcdMatrixBEntry = Entry(abcdMatrixFrame, font=("Helvetica", 10), width=7, justify="right", state="disabled")
     abcdMatrixBEntry.grid(row=0, column=3, sticky="e")
-    abcdMatrixCEntry = Entry(abcdMatrixFrame, font=("Helvetica", 10), width=7, justify="right")
+    abcdMatrixCEntry = Entry(abcdMatrixFrame, font=("Helvetica", 10), width=7, justify="right", state="disabled")
     abcdMatrixCEntry.grid(row=1, column=1, sticky="w")
-    abcdMatrixDEntry = Entry(abcdMatrixFrame, font=("Helvetica", 10), width=7, justify="right")
+    abcdMatrixDEntry = Entry(abcdMatrixFrame, font=("Helvetica", 10), width=7, justify="right", state="disabled")
     abcdMatrixDEntry.grid(row=1, column=3, sticky="e")
+    
+    calculateBtn = Button(root, text="Extraction", font=("Helvetica", 10), background="light gray", command=lambda:parametersExtraction())
+    calculateBtn.grid(row=2, column=0)
     
     root.mainloop()
     
