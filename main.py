9c52f2777662c6a5083b37bbe497a98967de6dd7
Lorenzo Carrano <lorenzoc7297@gmail.com>
2022-07-30 12:17:54 +0200

    Input panel completed

--- main.py
+++ main.py
@@ -13,10 +13,16 @@ def main():
     InputFrame = LabelFrame(root, text="Input", font=("Helvetica", 10), background=frame_background_color)
     InputFrame.grid(row=0,column=0)
     #operating frequency
-    opFreqLabel = Label(InputFrame, text="Op Frequency", font=("Helvetica", 10), background=options_background_color, justify='left')
-    opFreqLabel.grid(row=0, column=0)
-    opFreqEntr = Entry(InputFrame, font=("Helvetica", 10), width=7, justify="right")
-    opFreqEntr.grid(row=0, column=1)
+    ParameterFrame = LabelFrame(InputFrame, text="Electrical Parameters", font=("Helvetica", 10), background=options_background_color)
+    ParameterFrame.grid(row=0, column=0)
+    opFreqLabel = Label(ParameterFrame, text="Op Frequency", font=("Helvetica", 10), background=options_background_color, justify='left')
+    opFreqLabel.grid(row=0, column=0, sticky='w')
+    chImpedanceLabel = Label(ParameterFrame, text="Ch. Impedance", font=("Helvetica", 10), background=options_background_color, justify='left')
+    chImpedanceLabel.grid(row=1, column=0, sticky='w')
+    opFreqEntr = Entry(ParameterFrame, font=("Helvetica", 10), width=7, justify="right")
+    opFreqEntr.grid(row=0, column=1, sticky='w')
+    chImpedanceEntr = Entry(ParameterFrame, font=("Helvetica", 10), width=7, justify="right")
+    chImpedanceEntr.grid(row=1, column=1, sticky='w')
     #scattering matrix
     #labels
     scatteringMatrixFrame = LabelFrame(InputFrame, text="Scattering Matrix", font=("Helvetica", 10), background=options_background_color)
@@ -38,6 +44,31 @@ def main():
     scatteringMatrixS21Entry.grid(row=1, column=1, sticky="w")
     scatteringMatrixS22Entry = Entry(scatteringMatrixFrame, font=("Helvetica", 10), width=7, justify="right")
     scatteringMatrixS22Entry.grid(row=1, column=3, sticky="e")
+    
+    #output frame
+    OutputFrame = LabelFrame(root, text="Output", font=("Helvetica", 10), background=frame_background_color)
+    OutputFrame.grid(row=0,column=1)
+    #ABCD matrix
+    #labels
+    abcdMatrixFrame = LabelFrame(OutputFrame, text="ABCD Matrix", font=("Helvetica", 10), background=options_background_color)
+    abcdMatrixFrame.grid(row=1, column=0)
+    abcdMatrixALabel = Label(abcdMatrixFrame, text="A", font=("Helvetica", 10), background=options_background_color, justify='left')
+    abcdMatrixALabel.grid(row=0, column=0)
+    abcdMatrixBLabel = Label(abcdMatrixFrame, text="B", font=("Helvetica", 10), background=options_background_color, justify='left')
+    abcdMatrixBLabel.grid(row=0, column=2)
+    abcdMatrixCLabel = Label(abcdMatrixFrame, text="C", font=("Helvetica", 10), background=options_background_color, justify='left')
+    abcdMatrixCLabel.grid(row=1, column=0)
+    abcdMatrixDLabel = Label(abcdMatrixFrame, text="D", font=("Helvetica", 10), background=options_background_color, justify='left')
+    abcdMatrixDLabel.grid(row=1, column=2)
+    #parameter entries
+    abcdMatrixAEntry = Entry(abcdMatrixFrame, font=("Helvetica", 10), width=7, justify="right")
+    abcdMatrixAEntry.grid(row=0, column=1, sticky="w")
+    abcdMatrixBEntry = Entry(abcdMatrixFrame, font=("Helvetica", 10), width=7, justify="right")
+    abcdMatrixBEntry.grid(row=0, column=3, sticky="e")
+    abcdMatrixCEntry = Entry(abcdMatrixFrame, font=("Helvetica", 10), width=7, justify="right")
+    abcdMatrixCEntry.grid(row=1, column=1, sticky="w")
+    abcdMatrixDEntry = Entry(abcdMatrixFrame, font=("Helvetica", 10), width=7, justify="right")
+    abcdMatrixDEntry.grid(row=1, column=3, sticky="e")
     
     root.mainloop()
     
