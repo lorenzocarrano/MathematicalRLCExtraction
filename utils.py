c6bb6eedb7dfdd316e8a17725327dff182133fb0
Lorenzo Carrano <lorenzoc7297@gmail.com>
2022-08-20 12:28:40 +0200

    product between matrixes works

--- utils.py
+++ utils.py
@@ -1,13 +1,14 @@
 def matrixProduct(m1,m2):
     #limited to 2x2 case
     res = []
-    t = m1[0] * m2[0] + m1[1] + m2[2]
-    res.append(t)
-    t = m1[0] * m2[1] + m1[1] + m2[3]
-    res.append(t)
-    t = m1[2] * m2[0] + m1[3] + m2[2]
-    res.append(t)
-    t = m1[2] * m2[1] + m1[3] + m2[3]
-    res.append(t)
-
-    print(res)
+    if len(m1) == 4 and len(m2) == 4:
+        t = m1[0] * m2[0] + m1[1] * m2[2]
+        res.append(t)
+        t = m1[0] * m2[1] + m1[1] * m2[3]
+        res.append(t)
+        t = m1[2] * m2[0] + m1[3] * m2[2]
+        res.append(t)
+        t = m1[2] * m2[1] + m1[3] * m2[3]
+        res.append(t)
+    
+    return res
